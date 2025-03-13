86a7944754cf5aed0d5bcc0938501d4cb3dcb091
Avhad-Enterprises <awesomeboysboys@gmail.com>
2025-03-13 10:12:45 +0530

    Update

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -31,7 +31,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'sportsaga_db_aegit',
+        'database'     => 'sportsaga_db_adityapatil',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
